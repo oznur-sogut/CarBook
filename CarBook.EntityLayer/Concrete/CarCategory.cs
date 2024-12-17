2f6b866a9f18f7460bed223a95f9be0e33eb93b0
Öznur <oznur.sgt27@gmail.com>
2024-12-17 14:13:59 +0300

    Add project files.

---
+++ CarBook.EntityLayer/Concrete/CarCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.EntityLayer.Concrete
+{
+    public class CarCategory
+    {
+        public int CarCategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public string CategoryDescription { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
---
+++ CarBook.EntityLayer/Concrete/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.EntityLayer.Concrete
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public string LocationName { get; set; }
+        public bool LocationStatus { get; set; }
+
+    }
+}
