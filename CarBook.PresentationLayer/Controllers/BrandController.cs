9ba707e10f2e78ffe1b614c2b279a6b22b5e3943
Öznur <oznur.sgt27@gmail.com>
2024-12-25 16:19:35 +0300

    master

--- CarBook.PresentationLayer/Controllers/BrandController.cs
+++ CarBook.PresentationLayer/Controllers/BrandController.cs
@@ -1,4 +1,5 @@
 ﻿using CarBook.BusinessLayer.Abstract;
+using CarBook.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.PresentationLayer.Controllers
@@ -16,6 +17,37 @@ namespace CarBook.PresentationLayer.Controllers
 		{
 			var value= _brandService.TGetListAll();
 			return View(value);
+		}
+		[HttpGet]
+		public IActionResult AddBrand()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddBrand(Brand brand)
+		{
+
+			_brandService.TInsert(brand);
+			return RedirectToAction("BrandIndex");
+		}
+		public IActionResult DeleteBrand(int id)
+		{
+			var value= _brandService.TGetByID(id);
+			_brandService.TDelete(value);
+			return RedirectToAction("BrandIndex");
+		}
+		[HttpGet]
+		public IActionResult UpdateBrand(int id)
+		{
+			var value = _brandService.TGetByID(id);
+			_brandService.TUpdate(value);
+			return View(value);
+		}
+		[HttpPost]
+		public IActionResult UpdateBrand(Brand brand)
+		{
+			_brandService.TUpdate(brand);
+			return RedirectToAction("BrandIndex");
 		}
 	}
 }
